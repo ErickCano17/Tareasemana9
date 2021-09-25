7b6424851eebd6541295c934e02956eb781fab65
Erick Cano <Erick Cano@ERICKCANO>
2021-09-26 00:17:47 +0200

    Agregar archivos de proyecto.

---
+++ Tareasemana9/Bank.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tareasemana9
+{
+    class Bank
+    {
+        //Metodo de propiedad
+        private int _BankId;
+        private string _branchname;
+        private string _direction;
+        private string _Clientnumber;
+
+        //Metodo de propiedad autoimplementada 
+        public string _Phonenumber{ get; set; }
+
+        //Metodo constructor
+        public Bank() //Metodo vacio 
+        { }
+
+        //Metodo de sobrecarga de constructor 
+        public Bank(int BankId, string branchname, string direction, string Clientnumber, string Phonenumber)
+        {
+            _BankId = BankId;
+            _branchname = branchname;
+            _direction = direction;
+            _Clientnumber = Clientnumber;
+            _Phonenumber = Phonenumber;
+        }
+
+        //Id del banco
+        public int getBankId()
+        {
+            return _BankId;
+        }
+
+        public void setBankId(int BankId)
+        {
+            _BankId = BankId;
+        }
+
+        //nombre de la sucursal
+        public string getbranchname()
+        {
+            return _branchname;
+        }
+
+        public void setbranchname(string branchname)
+        {
+            _branchname = branchname;
+        }
+
+        //direccion
+        public string getdirection()
+        {
+            return _direction;
+        }
+
+        public void setdirection(string direction)
+        {
+            _direction = direction;
+        }
+
+        //numero del cliente
+        public string getClientnumber()
+        {
+            return _Clientnumber;
+        }
+
+        public void setClientnumber(string Clientnumber)
+        {
+            _Clientnumber = Clientnumber;
+        }
+
+        //Metodo para mostrar información del Banco
+        public string getBankInfo()
+        {
+            return "ID:   " + _BankId + "Nombre del la sucursal:  " + _branchname + "Direccion:  " + _direction + "Numero del cliente:  " + _Clientnumber + "Numero de telefono:  " + _Phonenumber;
+
+        }
+
+    }
+}
+
+    
+
---
+++ Tareasemana9/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tareasemana9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // creamos la instancia del banco
+            Bank bank = new Bank();
+            //Creamos un objeto
+
+            //Asignar valores a las propiedades
+            bank.setBankId(1);
+            bank.setbranchname("");
+            bank.setdirection("");
+            bank.setClientnumber("");
+
+
+            //Asignar valores a propiedades autoimplementadas 
+
+            bank._Phonenumber = ("");
+            //Mostrar datos del banco
+
+
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            //Instancia de la clase utilizando constructores 
+
+            //Utilizando constructor vacio 
+            Bank bank = new Bank();
+            bank.setBankId(Convert.ToInt32(txtID.Text));
+            bank.setbranchname(txtBranchname.Text);
+            bank.setdirection(txtDirection.Text);
+            bank.setClientnumber ( txtClientnumber.Text);
+            bank._Phonenumber = txtPhonenumber.Text;
+
+
+            //Constructor que inicializa todas las propiedades
+            Bank bank1 = new Bank(Convert.ToInt32(txtID.Text),
+                txtBranchname.Text, txtDirection.Text, txtClientnumber.Text, txtPhonenumber.Text);
+
+
+            //agregar los datos a listbox
+            lstbankInfo.Items.Add("Mediante constructor vacío: " + bank.getBankInfo());
+            lstbankInfo.Items.Add("Mediante constructor inicializando las propiedades: " + bank.getBankInfo());
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+    
+
